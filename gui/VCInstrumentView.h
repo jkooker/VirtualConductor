6a5fc925589e572e781dd878b171cf66b3a335d6
John Kooker <jkooker@gmail.com>
2010-03-13 13:44:07 -0800

    add first instrument view, with images.

---
+++ gui/VCInstrumentView.h
@@ -0,0 +1,18 @@
+//
+//  VCInstrumentView.h
+//  VirtualConductor
+//
+//  Created by John Kooker on 3/13/10.
+//  Copyright 2010 John Kooker. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface VCInstrumentView : NSView {
+    IBOutlet NSImageView *image;
+    IBOutlet NSLevelIndicator *level;
+    IBOutlet NSTextField *text;
+}
+
+@end
---
+++ gui/VCInstrumentView.m
@@ -0,0 +1,26 @@
+//
+//  VCInstrumentView.m
+//  VirtualConductor
+//
+//  Created by John Kooker on 3/13/10.
+//  Copyright 2010 John Kooker. All rights reserved.
+//
+
+#import "VCInstrumentView.h"
+
+
+@implementation VCInstrumentView
+
+- (id)initWithFrame:(NSRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect {
+    // Drawing code here.
+}
+
+@end
